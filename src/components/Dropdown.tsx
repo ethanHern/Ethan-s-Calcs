ee659c30d1387516af8523b58bf5471b9da6aa79
Ethan Hernandez <mrcereal295@gmail.com>
2026-09-07 22:38:52 -0400

    Introduced vector calculations

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+
+export default function Dropdown() {
+    const [isOpen, SetIsOpen] = useState<boolean>(false);
+
+
+}
--- src/utils/matrix.ts
+++ src/utils/matrix.ts
@@ -1,7 +1,8 @@
+import type { Vector } from "./vector";
+
 /*A custom type that represents a 2D array*/
 export type Matrix = number[][];
 export type StringMatrix = string[][];
-export type Vector = number[];
 
 export class MatrixMethods {
     constructor() {}
@@ -85,70 +86,7 @@ export class MatrixMethods {
     
 }
 
-export class VectorMethods {
 
-    constructor() {}
-
-    public copy(vector: Vector): Vector {
-        return vector.slice(0);
-    }
-
-    public to_matrix(vector: Vector): Matrix {
-        let matrix: Matrix = [];
-        for (let i = 0; i < vector.length; i++) {
-            matrix.push([vector[i]])
-        }
-        return matrix;
-    }
-
-
-    public vectors_to_matrix(vectors: Vector[]): Matrix {
-        // Verify all vectors are the same length
-        const vector_length = vectors[0].length;
-        for (let i = 0; i < vectors.length; i++) {
-            if (vectors[i].length != vector_length) { return [];}
-        }
-
-        let matrix: Matrix = this.to_matrix(vectors[0]);
-        if (vectors.length == 1) {return matrix;}
-        else {
-            for (let col = 1; col < vectors.length; col++) {
-                matrix = AddColumn(matrix, vectors[col]);
-            }
-            return matrix;
-        }
-    }
-
-    public subtract(vec1: Vector, vec2: Vector): Vector {
-        let result = this.copy(vec1);
-        for (let i = 0; i < result.length; i++) {
-            result[i] -= vec2[i];
-        }
-        return result;
-    }
-
-    public magnitude(vector: Vector): number {
-        let mag = 0;
-        for (let i = 0; i < vector.length; i++) {
-            mag += Math.abs(vector[i]) ** 2;
-        }
-        return Math.sqrt(mag);
-    }
-
-    public transpose(vector: Vector): Matrix {
-        const mMethods = new MatrixMethods();
-        return mMethods.transpose(this.to_matrix(vector));
-    }
-
-    public normalize(vector: Vector): Vector {
-        const mag = this.magnitude(vector);
-        let result = this.copy(vector);
-        for (let i = 0; i < vector.length; i++) {
-            result[i] = result[i] / mag;
-        }
-        return result;
-    }
-}
 
 function isMatrix(input: any, type: "number" | "string" = "number"): input is Matrix {
     return (Array.isArray(input) && 
